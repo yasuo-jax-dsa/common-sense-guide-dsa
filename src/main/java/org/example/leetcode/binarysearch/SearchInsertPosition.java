188383cffb8672e032a9f77960c6df1de17281ac
Shahab <bagheri.shahab94@gmail.com>
2024-06-08 00:50:41 +0200

    bugfix: BinarySearch: SearchInsertPosition
    fixed code related to unresolved test case with values {1,3} and target: 0

--- src/main/java/org/example/leetcode/binarysearch/SearchInsertPosition.java
+++ src/main/java/org/example/leetcode/binarysearch/SearchInsertPosition.java
@@ -4,36 +4,39 @@ public class SearchInsertPosition {
 
     public static void main(String[] args) {
 
-        var values = new int[]{1, 3, 5, 6};
-        var target = 2;
+        var values = new int[]{1, 3};
+        var target = 1;
 
         var res = doSearchInsert(values, target);
         System.out.println(res);
     }
 
-    private static int doSearchInsert(int[] values, int target) {
+    private static int doSearchInsert(int[] nums, int target) {
 
-        int upperBound = values.length - 1;
+        int upperBound = nums.length - 1;
         int lowerBound = 0;
         int middleIndex;
 
         while (upperBound >= lowerBound) {
+
             middleIndex = lowerBound + (upperBound - lowerBound) / 2;
 
+            if (target == nums[middleIndex]) {
+                return middleIndex;
+            }
+
             if (upperBound == lowerBound) {
-                if (target < values[middleIndex]) {
+                if (target < nums[middleIndex]) {
                     return middleIndex;
                 } else {
                     return middleIndex + 1;
                 }
             }
 
-            if (target > values[middleIndex]) {
+            if (target > nums[middleIndex]) {
                 lowerBound = middleIndex + 1;
-            } else if (target < values[middleIndex]) {
-                upperBound = middleIndex - 1;
-            } else if (target == values[middleIndex]) {
-                return middleIndex;
+            } else if (target < nums[middleIndex]) {
+                upperBound = middleIndex;
             }
         }
         return -1;
