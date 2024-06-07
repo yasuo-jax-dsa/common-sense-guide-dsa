b5ada23f9711a92d4163832bee68ba94506d1481
Shahab <bagheri.shahab94@gmail.com>
2024-06-08 00:54:01 +0200

    indentation fixed

--- src/main/java/org/example/leetcode/binarysearch/FirstBadVersion.java
+++ src/main/java/org/example/leetcode/binarysearch/FirstBadVersion.java
@@ -2,28 +2,33 @@ package org.example.leetcode.binarysearch;
 
 public class FirstBadVersion {
 
-	public final int BAD_VERSION;
+    public final int BAD_VERSION;
 
-	public FirstBadVersion(int badVersion) {BAD_VERSION = badVersion;}
+    public FirstBadVersion(int badVersion) {
+        BAD_VERSION = badVersion;
+    }
 
-	public int findFirstBadVersion(int n) {
-		var start = 1; var end = n;
+    public int findFirstBadVersion(int n) {
+        var start = 1;
+        var end = n;
 
-		while (start <= end) {
-			var mid = start + ((end - start) / 2); if (isBadVersion(mid))
-				end = mid - 1;
-			else
-				start = mid + 1;
-		} return start;
-	}
+        while (start <= end) {
+            var mid = start + ((end - start) / 2);
+            if (isBadVersion(mid))
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        return start;
+    }
 
-	private boolean isBadVersion(int version) {
-		return BAD_VERSION <= version;
-	}
+    private boolean isBadVersion(int version) {
+        return BAD_VERSION <= version;
+    }
 
-	public static void main(String[] args) {
-		FirstBadVersion firstBadVersion = new FirstBadVersion(1);
+    public static void main(String[] args) {
+        FirstBadVersion firstBadVersion = new FirstBadVersion(1);
 
-		System.out.println(firstBadVersion.findFirstBadVersion(1));
-	}
+        System.out.println(firstBadVersion.findFirstBadVersion(1));
+    }
 }
